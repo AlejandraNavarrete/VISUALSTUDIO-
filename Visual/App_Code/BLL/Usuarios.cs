45e2ffcfcb751a1d92bf71050d2782cdf07c8eed
AlejandraNavarrete <42845894+AlejandraNavarrete@users.noreply.github.com>
2018-09-17 21:40:10 -0500

    Add files via upload

---
+++ Visual/App_Code/BLL/Usuarios.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL;
+
+/// <summary>
+/// Descripción breve de Usuario
+/// </summary>
+public class Usuarios
+{
+    public Usuarios()
+    {
+
+    }
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+        public static long InsertUsuario(UsuarioInfo info)
+    {
+        Usuarios usuario = new Usuarios();
+        usuario.Nombre = usuario.Nombre;
+        usuario.Apellido_Paterno = usuario.Apellido_Paterno;
+        usuario.Apellido_Materno = usuario.Apellido_Materno;
+        usuario.Contraseña = usuario.Contraseña;
+        usuario.Correo_electronico = usuario.Correo_electronico;
+    }
+    public class UsuarioInfo
+    {
+        public long ID_Usuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido_Paterno { get; set; }
+        public string Apellido_Materno { get; set; }
+        public string Contraseña { get; set; }
+        public string Correo_Electronico { get; set; }
+        public long ID_Videos { get; set; }
+        public long ID_Comentarios { get; set; }
+    }
+}
---
+++ Visual/Controles/UsuariosNuevoControl.ascx
@@ -0,0 +1,18 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="UsuariosNuevoControl.ascx.cs" Inherits="Controles_UsuariosNuevoControl" %>
+
+<asp:Label ID="Label2" runat="server" Text="NOMBRE"></asp:Label>
+<asp:TextBox ID="TextBox3" runat="server" style="margin-left: 1px" Width="322px"></asp:TextBox>
+<p>
+
+<asp:Label ID="Label1" runat="server" Text="APELLIDO PATERNO"></asp:Label>
+<asp:TextBox ID="TextBox2" runat="server" Width="240px"></asp:TextBox>
+</p>
+<p>
+<asp:CheckBox ID="CheckBox1" runat="server" />
+</p>
+<p>
+
+<asp:TextBox ID="TextBox1" runat="server" Height="125px" TextMode="MultiLine" Width="230px"></asp:TextBox>
+</p>
+
+
